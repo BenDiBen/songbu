6b334853169bb6c32b311a74e8aa292cc2a0d7b8
Ben Diesel <befkotze@gmail.com>
2024-08-26 18:33:34 +0200

    Song book conversion function

--- src/types/song-book-import.ts
+++ src/types/song-book-import.ts
@@ -1,3 +1,28 @@
+import { filter, groupBy, is, map, path, pathOr, pipe, toPairs } from "ramda";
+import type { SongBookColumnMapping } from "./song-book-column-mapping";
+
 export type ArtistName = string;
 export type SongTitle = string;
 export type SongBookImport = Record<ArtistName, SongTitle[]>;
+
+export const toSongBookImport = (
+	data: Record<string, string>[],
+	{ artist, title }: SongBookColumnMapping,
+) => {
+	const result: SongBookImport = {};
+
+	if (!artist || !title) {
+		return result;
+	}
+
+	const getArtist = pathOr("", [artist]);
+	const getTitle = path<string>([title]);
+	const groups = toPairs(groupBy(getArtist, data));
+
+	groups.forEach(([artist, items = []]) => {
+		const songs = pipe(map(getTitle), filter(is(String)))(items);
+		result[artist] = songs;
+	});
+
+	return result;
+};
