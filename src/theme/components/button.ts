76825f8027c5e1c5d51bdc766b02d8928e44ffbd
Ben Diesel <befkotze@gmail.com>
2024-09-11 18:26:10 +0200

    Improve disabled style

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -15,26 +15,52 @@ const gradient = defineStyle((props) => {
 		getColor("gray", "800"),
 	)(props);
 
-	return {
-		border: "2px solid",
-		borderColor: "transparent",
-		background: `linear-gradient(${bgColor}, ${bgColor}) padding-box, 
+	const defaultBackground = `linear-gradient(${bgColor}, ${bgColor}) padding-box, 
       linear-gradient(30deg, ${getColor(colorScheme, "400", 0.8)} 0%,
       ${getColor(colorScheme, "600", 0.8)} 20%,
       ${getColor(colorScheme, "300", 0.8)} 37%,
       ${getColor("gray", "800")} 58%,
       ${getColor("accent", "400", 0.65)} 73%,
-      ${getColor("accent", "600", 0.8)} 100%) border-box`,
-		shadow: `-10px 5px 30px -3px ${getColor(colorScheme, "500", 0.5)}, 10px -5px 30px -3px ${getColor("accent", "500", 0.5)}`,
-		_hover: {
-			background: `linear-gradient(${bgColor}, ${bgColor}) padding-box, 
+      ${getColor("accent", "600", 0.8)} 100%) border-box`;
+	const defaultShadow = `-10px 5px 30px -3px ${getColor(colorScheme, "500", 0.5)}, 10px -5px 30px -3px ${getColor("accent", "500", 0.5)}`;
+	const hoverBackground = `linear-gradient(${bgColor}, ${bgColor}) padding-box, 
       linear-gradient(30deg, ${getColor(colorScheme, "300", 0.8)} 0%,
       ${getColor(colorScheme, "200", 0.8)} 20%,
       ${getColor(colorScheme, "200", 0.8)} 37%,
       ${getColor("gray", "800")}  58%,
       ${getColor("accent", "200", 0.65)} 73%,
-      ${getColor("accent", "300", 0.8)} 100%) border-box`,
-			shadow: `-10px 5px 30px -3px ${getColor(colorScheme, "400", 0.6)}, 10px -5px 30px -3px ${getColor("accent", "400", 0.6)}`,
+      ${getColor("accent", "300", 0.8)} 100%) border-box`;
+	const hoverShadow = `-10px 5px 30px -3px ${getColor(colorScheme, "400", 0.6)}, 10px -5px 30px -3px ${getColor("accent", "400", 0.6)}`;
+
+	const defaultStyle = {
+		border: "2px solid",
+		borderColor: "transparent",
+		background: defaultBackground,
+		shadow: defaultShadow,
+	};
+
+	const hoverStyle = {
+		background: hoverBackground,
+		shadow: hoverShadow,
+	};
+
+	const disabledStyle = {
+		color: getColor("gray", "300", 0.8),
+		opacity: 0.7,
+	};
+
+	return {
+		...defaultStyle,
+		_hover: {
+			...hoverStyle,
+			_disabled: {
+				...hoverStyle,
+				...disabledStyle,
+			},
+		},
+		_disabled: {
+			...defaultStyle,
+			...disabledStyle,
 		},
 	};
 });
