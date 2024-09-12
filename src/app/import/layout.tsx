0b37778415c31670ab7741ead147c57efbf53396
Ben Diesel <befkotze@gmail.com>
2024-09-12 06:33:36 +0200

    Remove unused page

--- src/app/import/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithChildren } from "react";
-import { ImportProviders } from "./providers";
-
-const ImportLayout = ({ children }: PropsWithChildren) => {
-	return <ImportProviders>{children}</ImportProviders>;
-};
-
-export default ImportLayout;
--- src/app/import/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { DragProvider } from "@/components/contexts/drag-provider";
-import type { PropsWithChildren } from "react";
-
-export const ImportProviders = ({ children }: PropsWithChildren) => {
-	return <DragProvider>{children}</DragProvider>;
-};
