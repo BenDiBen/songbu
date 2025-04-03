32ba0d1c18c19890141b70abf3203df9983d9b42
Ben Diesel <befkotze@gmail.com>
2025-04-03 10:46:54 +0200

    Refactor slider

--- app/video-editor/components/sync-lyrics.tsx
+++ app/video-editor/components/sync-lyrics.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import { Slider, VStack } from "@chakra-ui/react";
+import { Tooltip } from "@/components/ui/tooltip";
+import { Box, Button, Slider, VStack, useSlider } from "@chakra-ui/react";
 import { useState } from "react";
 
-export const SyncLyrics = () => {
-	const values = [70, 75];
+export const SyncLyrics = ({ max = 300 }: { max?: number }) => {
+	const [value, setValue] = useState([50, 75]);
 	const [isMouseOver, setIsMouseOver] = useState(false);
+	const [isHoveringThumb, setIsHoveringThumb] = useState(false);
 	const [mouseThumbPosition, setMouseThumbPosition] = useState(0);
+	const showNewThumb = isMouseOver && !isHoveringThumb;
+	const slider = useSlider({
+		max,
+		value: [mouseThumbPosition, ...value],
+		step: 0.1,
+		onValueChangeEnd: (e) => setValue(e.value),
+	});
 
 	return (
 		<VStack>
-			<Slider.Root
-				thumbAlignment="center"
-				position="relative"
-				minW="2xl"
-				value={[mouseThumbPosition, ...values]}
-				max={100}
+			{value.join(",")}
+			<Button onClick={() => setValue([70, 75])}>Add</Button>
+			<Box
 				onMouseEnter={() => setIsMouseOver(true)}
 				onMouseLeave={() => setIsMouseOver(false)}
 				onMouseMove={(e) => {
@@ -26,27 +32,25 @@ export const SyncLyrics = () => {
 					const rect = e.currentTarget.getBoundingClientRect();
 					const percent = (e.clientX - rect.left) / rect.width;
 
-					setMouseThumbPosition(Math.max(Math.min(percent * 100, 100), 0));
+					setMouseThumbPosition(Math.max(Math.min(percent, 1), 0) * max);
 				}}
-				py={4}
 			>
-				<Slider.Control>
-					<Slider.Track />
-					<Slider.Thumb index={0} opacity={isMouseOver ? 1 : 0}>
-						<Slider.DraggingIndicator
-							layerStyle="fill.solid"
-							top="6"
-							rounded="sm"
-							px="1.5"
-						>
-							<Slider.ValueText />
-						</Slider.DraggingIndicator>
-					</Slider.Thumb>
-					{values.map((value, index) => (
-						<Slider.Thumb key={value} index={index + 1} />
-					))}
-				</Slider.Control>
-			</Slider.Root>
+				<Tooltip content="+" portalled>
+					<Slider.RootProvider
+						// thumbAlignment="center"
+						position="relative"
+						minW="2xl"
+						value={slider}
+						// onValueChange={(e) => setValues(e.value)}
+						py={4}
+					>
+						<Slider.Control>
+							<Slider.Track />
+							<Slider.Thumbs />
+						</Slider.Control>
+					</Slider.RootProvider>
+				</Tooltip>
+			</Box>
 		</VStack>
 	);
 };
